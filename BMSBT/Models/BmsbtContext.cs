386a57752f1d87943fce3e0a129297b2dcde5d74
msghauri3 <msghauri3@yahoo.com>
2026-02-12 16:46:06 +0500

    New User Ali

--- BMSBT/Models/BmsbtContext.cs
+++ BMSBT/Models/BmsbtContext.cs
@@ -55,7 +55,7 @@ public partial class BmsbtContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=172.20.228.2;Database=BMSBT;User ID=sa;Password=Pakistan@786;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=172.20.228.2;Database=BMSSafariRwp;User ID=sa;Password=Pakistan@786;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
